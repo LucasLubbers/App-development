19fc886ff64bd67dcd271040dcebdf098a69643b
Aaron de Bruin <aaron.de.bruin@student.nhlstenden.com>
2025-05-09 20:03:11 +0200

    add beginning of navbar

--- app/src/main/java/com/nhlstenden/WorkoutNHLStenden/MainActivity.kt
+++ app/src/main/java/com/nhlstenden/WorkoutNHLStenden/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.nhlstenden.WorkoutNHLStenden
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.snackbar.Snackbar
 import com.nhlstenden.WorkoutNHLStenden.databinding.ActivityMainBinding
 
@@ -25,7 +28,26 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         val navController = navHostFragment.navController
-
+        
+        // Set up bottom navigation
+        binding.bottomNavigation.setupWithNavController(navController)
+        
+        // Completely disable ripple effect
+        binding.bottomNavigation.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Don't show any visual effect
+                    v.parent.requestDisallowInterceptTouchEvent(true)
+                }
+            }
+            false
+        }
+        
+        // Disable any animation or indicator on menu items
+        val menuView = binding.bottomNavigation.getChildAt(0)
+        menuView?.isActivated = false
+        menuView?.background = null
+        
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
