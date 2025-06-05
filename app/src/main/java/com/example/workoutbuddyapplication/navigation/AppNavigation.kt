f7fb600c12b10703c48fe184be9fb6f45e870e55
LucasLubbers <lucas.lubbers@student.nhlstenden.com>
2025-06-05 19:20:48 +0200

    feat: Added a new notification settings screen, updated the androidmanifest for notification settings, made notification service. Made a temporary test notification to show that it works and there's a setting to turn notifications on/off

--- app/src/main/java/com/example/workoutbuddyapplication/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/workoutbuddyapplication/navigation/AppNavigation.kt
@@ -29,6 +29,7 @@ sealed class Screen(val route: String) {
     object Stats : Screen("stats")
     object Goals : Screen("goals")
     object Settings : Screen("settings")
+    object NotificationSettings : Screen("notification_settings")
     object StartWorkout : Screen("start_workout")
     object RunningWorkout : Screen("running_workout")
     object StrengthWorkout : Screen("strength_workout")
@@ -100,6 +101,9 @@ fun AppNavigation(navController: NavHostController) {
         }
         composable(Screen.Settings.route) {
             SettingsScreen(navController = navController)
+        }
+        composable(Screen.NotificationSettings.route) {
+            NotificationSettingsScreen(navController = navController)
         }
         composable(
             route = "workoutDetail/{workoutId}/{selectedTabIndex}",
